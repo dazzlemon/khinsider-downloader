dec91a2ad26a76c0921ceb2efd63ed59d5bf3ab1
dazzlemon <daanill13470@gmail.com>
2023-12-13 01:05:07 +0200

    extract links

--- src/fetch_html.py
+++ src/fetch_html.py
@@ -1,18 +1,44 @@
 """
-TODO:
+fetch_html.py
 """
 
+from http import HTTPStatus
 import requests
+from bs4 import BeautifulSoup
+from functional import seq
+
 
 def fetch_html(url, timeout=5):
     """
-    TODO:
+    Fetch HTML content from a given URL.
     """
-    response = requests.get(url, timeout=timeout)
+    try:
+        response = requests.get(url, timeout=timeout)
 
-    if response.status_code == 200:
-        html_content = response.text
-        return html_content
+        status = response.status_code
+        if status == HTTPStatus.OK:
+            html_content = response.text
+            return html_content
 
-    print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
-    return None
+        print(f"Failed to retrieve the webpage. Code: {status}")
+        return None
+    except requests.exceptions.RequestException as error:
+        print(f"An error occurred: {error}")
+        return None
+
+
+def extract_hrefs(html_content):
+    """
+    Extract href values from td elements with class "playlistDownloadSong."
+    """
+    if html_content is None:
+        return []
+
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    return seq(
+        soup.find_all('td', class_='playlistDownloadSong'),
+    )\
+        .filter(lambda td: td.a)\
+        .map(lambda td: td.a['href'])\
+        .to_list()
--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
 """
-MAIN
+main.py
 """
 
 import sys
-from fetch_html import fetch_html
+from fetch_html import fetch_html, extract_hrefs
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python script.py <URL>")
+        print("Usage: python main.py <URL>")
         sys.exit(1)
 
     URL = sys.argv[1]
     html_content = fetch_html(URL)
 
     if html_content:
-        print(html_content)
+        hrefs = extract_hrefs(html_content)
+        print("Extracted HREFs:")
+        for href in hrefs:
+            print(href)
