d5d5a39125cbf041bca5388f902e707d72036ec6
dazzlemon <daanill13470@gmail.com>
2023-12-13 02:29:26 +0200

    refactor

--- src/extract_hrefs.py
+++ src/extract_hrefs.py
@@ -6,51 +6,41 @@ from bs4 import BeautifulSoup
 from functional import seq
 
 
-def extract_song_pages_paths(html_content):
+def extract_hrefs(html_content, selector, class_name, link_extractor=lambda a: a):
     """
-    Extract path to pages, where download links are.
+    Extract links based on the provided selector and class.
     """
     if html_content is None:
         return []
 
     soup = BeautifulSoup(html_content, 'html.parser')
 
-    links_elements = seq(
-        soup.find_all('td', class_='playlistDownloadSong'),
+    return seq(
+        soup.find_all(selector, class_=class_name)
     )\
-			  .map(lambda td: td.a)\
-        .to_list()
+			.map(link_extractor)\
+			.filter(not_none)\
+			.map(lambda a: a['href'])\
+			.filter(not_none)\
+			.to_list()
 
-    return extract_hrefs(links_elements)
+
+def extract_song_pages_paths(html_content):
+    """
+    Extract path to pages, where download links are.
+    """
+    def extractor(html_td):
+        return html_td.a
+    return extract_hrefs(html_content, 'td', 'playlistDownloadSong', extractor)
 
 
 def extract_download_links(html_content):
     """
     Extract download links.
     """
-    if html_content is None:
-        return []
-
-    soup = BeautifulSoup(html_content, 'html.parser')
-
-    links_elements = seq(
-        soup.find_all('span', class_='songDownloadLink')
-    )\
-        .map(lambda span: span.find_previous('a'))\
-        .to_list()
-
-    return extract_hrefs(links_elements)
-
-
-def extract_hrefs(links_elements):
-    """
-    Given a list of <a>'s, extract their hrefs
-    """
-    return seq(links_elements)\
-        .filter(not_none)\
-        .map(lambda a: a['href'])\
-        .filter(not_none)\
-        .to_list()
+    def extractor(span):
+        return span.find_previous('a')
+    return extract_hrefs(html_content, 'span', 'songDownloadLink', extractor)
 
 
 def not_none(something):
