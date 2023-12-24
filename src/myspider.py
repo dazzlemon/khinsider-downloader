44293e54f1acdfe685b938a1b9533e67ab73992a
dazzlemon <daanill13470@gmail.com>
2023-12-24 12:46:59 +0200

    almost

--- src/myspider.py
+++ src/myspider.py
@@ -7,28 +7,31 @@ class MySpider(scrapy.Spider):
         'LOG_LEVEL': 'ERROR',
         'CONCURRENT_REQUESTS': 8,
         'CONCURRENT_REQUESTS_PER_DOMAIN': 8,
+        'ITEM_PIPELINES': {
+            'scrapy.pipelines.files.FilesPipeline': 1,
+        },
     }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.start_url = kwargs.get('start_url')
         self.links = []
+        self.custom_settings['FILES_STORE'] = kwargs.get('path', None)
 
     def start_requests(self):
-        if not self.start_url:
+        if not self.start_url or not self.custom_settings['FILES_STORE']:
             print(
                 'Usage: scrapy runspider myspider.py '
                 '-a start_url=<URL> '
+                '-a path=<PATH>'
             )
-
-            print(self.start_url)
         else:
             yield scrapy.Request(url=self.start_url, callback=self.parse_main_page)
 
     def parse_main_page(self, response):
         cover_art_links = response.xpath('//div[@class="albumImage"]/a/@href').getall()
         for link in cover_art_links:
-            print(link)
+            yield {'file_urls': [link]}
 
         song_pages_paths = response.xpath('//td[@class="playlistDownloadSong"]/a/@href').getall()
         for path in song_pages_paths:
@@ -37,4 +40,4 @@ class MySpider(scrapy.Spider):
     def parse_song_page(self, response):
         links = response.xpath('//span[@class="songDownloadLink"]/../@href').getall()
         flac_link = next(filter(lambda link: link.endswith('.flac'), links), None)
-        print(flac_link or next(iter(links), None))
+        yield {'file_urls': [flac_link or next(iter(links), None)]}
