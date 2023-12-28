75b14e427bc5b12b7ccc990ba7af19585890eb66
dazzlemon <30831873+dazzlemon@users.noreply.github.com>
2023-12-28 06:57:31 +0200

    fix run.bat

--- run.bat
+++ run.bat
@@ -3,9 +3,10 @@
 for %%I in ("%~dp0.") do set "script_dir=%%~fI"
 
 set "start_url=%1"
-shift
-
 set "PYTHONPATH=%PYTHONPATH%;%script_dir%\src"
 
+set "remaining_args=%*"
+set "remaining_args=%remaining_args:* =%"
+
 @echo on
-scrapy runspider %script_dir%\src\spider.py -a start_url=%start_url% %*
+scrapy runspider %script_dir%\src\spider.py -a start_url=%start_url% %remaining_args%
