eb9286634506671ef24245d1326866b0aef89b38
dazzlemon <daanill13470@gmail.com>
2023-12-28 02:47:39 +0200

    simplify

--- src/pipeline.py
+++ src/pipeline.py
@@ -6,17 +6,15 @@ from parfive import Downloader
 class Pipeline(FilesPipeline):
     @classmethod
     def from_crawler(cls, crawler, *args, **kwargs):
-        cls.FILES_STORE = crawler.settings.get('FILES_STORE')
+        cls.FILES_STORE = crawler.settings['FILES_STORE']
 
         return super().from_crawler(crawler, *args, **kwargs)
 
-    def open_spider(self, _):
-        self.downloader = Downloader()
+    def open_spider(self, _): self.downloader = Downloader()
 
     def process_item(self, item, _):
-        file_url = item.get(self.files_urls_field, [])[0]
+        file_url = item[self.files_urls_field][0]
         filename = unquote(os.path.basename(urlparse(file_url).path))
         self.downloader.enqueue_file(file_url, filename=filename, path=self.FILES_STORE)
 
-    def close_spider(self, _):
-        self.downloader.download()
+    def close_spider(self, _): self.downloader.download()
